bfcf96cc9ebf44bc2767b6ba5968cf5ef285d2f1
ccguerreroc <cc.guerreroc@lanuevaamerica.edu.co>
2026-09-07 16:07:29 -0500

    Fix rutas Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { useState, useEffect } from 'react'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './routes/index.jsx'
+import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login/Login.jsx'
+import Home from './pages/Home/Home.jsx'
+import Solicitudes from './pages/Home/Solicitudes.jsx'
+import Administrador from './pages/Administrador/Administrador.jsx'
+import Conductor from './pages/Conductor/Conductor.jsx'
 import Loader from './loader/Loader.jsx'
 
 export default function App() {
@@ -13,7 +17,13 @@ export default function App() {
 
   return (
     <>
-      <RouterProvider router={router} />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/inicio" element={<Home />} />
+        <Route path="/solicitudes" element={<Solicitudes />} />
+        <Route path="/administrador" element={<Administrador />} />
+        <Route path="/conductor" element={<Conductor />} />
+      </Routes>
       {loading && <Loader />}
     </>
   )
